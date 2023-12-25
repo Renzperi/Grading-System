b9c941eb44ee3e60860e7f9a8223efdef9bdd7bb
Renzperi Inandan <inandanrenzperi09@gmail.com>
2023-12-25 12:43:07 +0800

    v0.1.20 - Canvas Dimension Set

--- Online Grading System/static/scripts/script.js
+++ Online Grading System/static/scripts/script.js
@@ -1,2 +1,8 @@
-const canvas = document.getElementById("myCanvas");
-const context = canvas.getContext("2d");
+const myCanvas = document.getElementById("myCanvas");
+const context = myCanvas.getContext("2d");
+myCanvas.width = window.innerWidth;
+myCanvas.height = window.innerHeight;
+
+context.beginPath();
+context.arc(100, 100, 100, 0, 2 * Math.PI);
+context.stroke();
