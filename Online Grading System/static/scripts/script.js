d5551bea6c54e78336756817fe81006bfc9534f0
Renzperi Inandan <inandanrenzperi09@gmail.com>
2023-12-25 13:29:52 +0800

    v0.1.21 - Canvas animation not finished

--- Online Grading System/static/scripts/script.js
+++ Online Grading System/static/scripts/script.js
@@ -3,6 +3,57 @@ const context = myCanvas.getContext("2d");
 myCanvas.width = window.innerWidth;
 myCanvas.height = window.innerHeight;
 
-context.beginPath();
-context.arc(100, 100, 100, 0, 2 * Math.PI);
-context.stroke();
+function Circle(x, y, radius, speed, color) {
+  this.xPos = x;
+  this.yPos = y;
+  this.radius = radius;
+  this.speed = speed;
+  this.color = color;
+
+  this.draw = function () {
+    context.fillStyle = this.color;
+    context.beginPath();
+    context.arc(this.xPos, this.yPos, this.radius, 0, 2 * Math.PI);
+    context.stroke();
+    context.fill();
+  };
+
+  this.move = function () {
+    if (this.xPost + this.radius > innerWidth || this.xPos - this.radius < 0) {
+      this.speed = -this.speed;
+    }
+
+    if (this.yPos + this.radius > innerHeight || this.yPos - this.radius < 0) {
+      this.speed = -this.speed;
+    }
+
+    const direction = [-1, 1];
+
+    this.xPos += direction[Math.floor(Math.random() * 2)] * this.speed;
+    this.yPos += direction[Math.floor(Math.random() * 2)] * this.speed;
+
+    this.draw();
+  };
+}
+
+const circleArray = [];
+const colors = ["#573b8b", "#425898", "#212874", "#252c62"];
+for (let i = 0; i < 500; i++) {
+  let radius = 20;
+  let x = myCanvas.width / 2;
+  let y = myCanvas.height / 2;
+  let speed = Math.random() * 9;
+  let color = colors[Math.floor(Math.random * colors.length)];
+  circleArray.push(new Circle(x, y, radius, speed, color));
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  context.clearRect(0, 0, innerWidth, innerHeight);
+
+  for (let i = 0; i < circleArray.length; i++) {
+    circleArray[i].move();
+  }
+}
+
+animate();
